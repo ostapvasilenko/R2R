d86e9b9bd88161b7345ad2166ee22fd537b42e71
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-08-17 22:57:28 +0300

    make section reviews

--- src/js/script.js
+++ src/js/script.js
@@ -26,3 +26,22 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+//Swiper Slider
+
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
