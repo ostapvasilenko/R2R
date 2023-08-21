2c8c9b6feb039403051d5dba28211bf9f4c6444d
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-08-21 11:07:23 +0300

    make slider in section reviews

--- src/js/script.js
+++ src/js/script.js
@@ -28,23 +28,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //Swiper Slider
 
-// let swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   keyboard: {
-//     enabled: true,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
+const reviewsSwiper = new Swiper(".reviews__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 5,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  initialSlide: 0, // Додайте цей рядок для початкового слайда 0 (перший слайд)
+  slidesPerView: calculateSlidesPerView(),
+});
+// Функція для розрахунку slidesPerView залежно від розміру вікна
+function calculateSlidesPerView() {
+  if (window.innerWidth < 768) {
+    return 1;
+  } else if (window.innerWidth < 1200) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
 
-let swiper = new Swiper(".mySwiper", {
+// Додаємо обробник події для оновлення слайдера при зміні розміру вікна
+window.addEventListener("resize", () => {
+  reviewsSwiper.params.slidesPerView = calculateSlidesPerView();
+  reviewsSwiper.update();
+});
+
+
+
+//Swiper Slider next
+
+const worksSwiper = new Swiper(".mySwiper", {
+  initialSlide: 1,  // Початковий слайд буде другим (індекс 1)
   effect: "coverflow",
   slidesPerView: 3,
   grabCursor: true,
@@ -62,44 +85,46 @@ let swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-swiper.slideTo(1, 0);
 
-function openPage(event, pageName) {
-  var i, tabContent, tabButton;
-  tabContent = document.getElementsByClassName("tab-content");
-  for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = "none";
-  }
-  tabButton = document.getElementsByClassName("tab-button");
-  for (i = 0; i < tabButton.length; i++) {
-    tabButton[i].className = tabButton[i].className.replace(" active", "");
-  }
-  document.getElementById(pageName).style.display = "block";
-  event.currentTarget.className += " active";
-}
+
+//Tabs
+
+// function openPage(event, pageName) {
+//   var i, tabContent, tabButton;
+//   tabContent = document.getElementsByClassName("tab-content");
+//   for (i = 0; i < tabContent.length; i++) {
+//     tabContent[i].style.display = "none";
+//   }
+//   tabButton = document.getElementsByClassName("tab-button");
+//   for (i = 0; i < tabButton.length; i++) {
+//     tabButton[i].className = tabButton[i].className.replace(" active", "");
+//   }
+//   document.getElementById(pageName).style.display = "block";
+//   event.currentTarget.className += " active";
+// }
 
 // За замовчуванням відкриємо першу сторінку
-document.getElementById("page2").style.display = "block";
-document.getElementsByClassName("tab-button")[0].className += " active";
+// document.getElementById("page1").style.display = "block";
+// document.getElementsByClassName("tab-button")[0].className += " active";
 
 
-
+//Tabs on page Servis
 
 const individualTab = document.getElementById('individualTab');
-    const singleTab = document.getElementById('singleTab');
-    const individualContent = document.getElementById('individualContent');
-    const singleContent = document.getElementById('singleContent');
+const singleTab = document.getElementById('singleTab');
+const individualContent = document.getElementById('individualContent');
+const singleContent = document.getElementById('singleContent');
 
-    individualTab.addEventListener('click', () => {
-      individualContent.classList.add('active');
-      singleContent.classList.remove('active');
-      individualTab.classList.add('active');
-      singleTab.classList.remove('active');
-    });
+individualTab.addEventListener('click', () => {
+  individualContent.classList.add('active');
+  singleContent.classList.remove('active');
+  individualTab.classList.add('active');
+  singleTab.classList.remove('active');
+});
 
-    singleTab.addEventListener('click', () => {
-      singleContent.classList.add('active');
-      individualContent.classList.remove('active');
-      singleTab.classList.add('active');
-      individualTab.classList.remove('active');
-    });
+singleTab.addEventListener('click', () => {
+  singleContent.classList.add('active');
+  individualContent.classList.remove('active');
+  singleTab.classList.add('active');
+  individualTab.classList.remove('active');
+});
